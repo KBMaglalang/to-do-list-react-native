f0f67503941933e9486fbd7895dbe90f7fbd3cb0
Kurt Maglalang <kbm126@live.com>
2022-10-16 16:19:55 -0700

    modifying colors and styling of text

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
+import { useState } from "react";
+
 import { StyleSheet, Text, View, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 // constants
 import COLORS from "./constants/colors";
 
+// components
+// item component
+// new item component
+// slide settings modal component
+
 export default function App() {
+  const [toDoList, setToDoList] = useState([]);
+
   return (
     <>
       <StatusBar style="auto" />
       <View style={styles.container}>
         <View style={styles.titleContainer}>
-          <Text>To Do</Text>
+          <Text style={styles.titleStyle}>To Do</Text>
         </View>
 
         <View style={styles.listContainer}>
-          <Text>Goals Go Here</Text>
+          <Text style={styles.listText}>Item Go Here</Text>
         </View>
 
         <View style={styles.buttonContainer}>
-          <Button title="Add New Goal" />
+          <Button title="Add New Item" color={COLORS.primary} />
         </View>
       </View>
     </>
@@ -35,6 +44,11 @@ const styles = StyleSheet.create({
     flex: 5,
     paddingVertical: 8,
   },
+  listText: {
+    fontSize: 24,
+    color: "white",
+  },
+
   buttonContainer: {
     paddingVertical: 16,
     borderTopWidth: 2,
@@ -45,5 +59,11 @@ const styles = StyleSheet.create({
     paddingBottom: 16,
     borderBottomWidth: 2,
     borderColor: COLORS.accent,
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "white",
+    // ! add font here later on
   },
 });
