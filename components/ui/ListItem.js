aabe66f859570f62e1ab23e0dab63fbce27d3cc3
Kurt Maglalang <kbm126@live.com>
2022-10-16 16:37:01 -0700

    reset style

--- components/ui/ListItem.js
+++ components/ui/ListItem.js
@@ -8,37 +8,4 @@ export default function ListItem() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-
-  listContainer: {
-    flex: 5,
-    paddingVertical: 8,
-  },
-  listText: {
-    fontSize: 24,
-    color: "white",
-  },
-
-  buttonContainer: {
-    paddingVertical: 16,
-    borderTopWidth: 2,
-    borderColor: COLORS.accent,
-  },
-  titleContainer: {
-    alignItems: "center",
-    paddingBottom: 16,
-    borderBottomWidth: 2,
-    borderColor: COLORS.accent,
-  },
-  titleStyle: {
-    fontWeight: "bold",
-    fontSize: 30,
-    color: "white",
-    // ! add font here later on
-  },
-});
+const styles = StyleSheet.create({});
