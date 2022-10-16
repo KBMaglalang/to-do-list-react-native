f5ece576c38891ca9af3f8dc481e5d9fcceeb71a
Kurt Maglalang <kbm126@live.com>
2022-10-16 16:09:17 -0700

    setup basic styling and content on app

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
+
+// constants
+import COLORS from "./constants/colors";
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <View>
-        <Text>To Do</Text>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text>To Do</Text>
+        </View>
 
-        <View>
+        <View style={styles.listContainer}>
           <Text>Goals Go Here</Text>
         </View>
-        <View>
+
+        <View style={styles.buttonContainer}>
           <Button title="Add New Goal" />
         </View>
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  listContainer: {
+    flex: 5,
+    paddingVertical: 8,
+  },
+  buttonContainer: {
+    paddingVertical: 16,
+    borderTopWidth: 2,
+    borderColor: COLORS.accent,
+  },
+  titleContainer: {
+    alignItems: "center",
+    paddingBottom: 16,
+    borderBottomWidth: 2,
+    borderColor: COLORS.accent,
+  },
+});
