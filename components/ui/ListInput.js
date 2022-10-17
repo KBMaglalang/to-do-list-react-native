b2e821960327ad3b1db29199c164fc26acd4390a
Kurt Maglalang <kbm126@live.com>
2022-10-17 04:40:08 -0700

    change background color of modal

--- components/ui/ListInput.js
+++ components/ui/ListInput.js
@@ -137,6 +137,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
+    backgroundColor: COLORS.background,
+    paddingHorizontal: 16,
   },
 
   titleContainer: {
