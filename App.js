00638aa812b56e2c40f713006c071b12f954ab0a
Kurt Maglalang <kbm126@live.com>
2022-10-16 21:42:44 -0700

    close modal once done

--- App.js
+++ App.js
@@ -53,6 +53,7 @@ export default function App() {
       ...currentList,
       { ...itemData, id: Math.random().toString() },
     ]);
+    cancelItemHandler();
   };
 
   const editItemHandler = (id) => {
