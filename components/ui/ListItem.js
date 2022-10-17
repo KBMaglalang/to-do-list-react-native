41515d5e9e22ac1426f52d4ded8242a77fc93283
Kurt Maglalang <kbm126@live.com>
2022-10-16 17:06:34 -0700

    add title and details text to item card

--- components/ui/ListItem.js
+++ components/ui/ListItem.js
@@ -1,11 +1,46 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button, Pressable } from "react-native";
 
-export default function ListItem() {
+// constants
+import COLORS from "../../constants/colors";
+
+export default function ListItem(props) {
   return (
-    <View>
-      <Text>test</Text>
+    <View style={styles.card}>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.itemTitle}>Title</Text>
+        <Text style={styles.itemText}>item</Text>
+      </View>
+      <View>
+        <Button title={"X"} color={COLORS.error} style={styles.deleteButton} />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "row",
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: COLORS.primary,
+  },
+
+  detailsContainer: {
+    flex: 5,
+    flexDirection: "column",
+    padding: 8,
+  },
+  itemTitle: {
+    fontSize: 24,
+    color: "white",
+  },
+  itemText: {
+    fontSize: 16,
+    color: "white",
+  },
+
+  deleteButton: {
+    flex: 1,
+    color: COLORS.error,
+  },
+});
